cd609faec2324808609133d57138d02925a69c1b
Joe van de Bilt <joe.van.de.bilt@codewizards.co.uk>
2024-12-16 17:07:46 +0000

    It bugged me so I fixed my C# code

---
+++ AdventOfCode.Shared/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Shared.Extensions;
+public static class DictionaryExtensions
+{
+    public static void UpsertEntry<T>(this IDictionary<T, long> dictionary, T key, long count = 1)
+    {
+        if (dictionary.ContainsKey(key))
+            dictionary[key]+=count;
+        else
+            dictionary.Add(key, count);
+
+    }
+}
--- AdventOfCode.Year2024/Days/11/DayElevenMain.cs
+++ AdventOfCode.Year2024/Days/11/DayElevenMain.cs
@@ -1,37 +1,59 @@
 ﻿using AdventOfCode.Shared.Base;
 using AdventOfCode.Shared.Enums;
-using System.Diagnostics;
+using AdventOfCode.Shared.Extensions;
 
 namespace AdventOfCode.Year2024.Days.DayEleven;
 public class DayElevenMain : AdventOfCodeDay
 {
     private const bool _debugging = true;
+
     public DayElevenMain() : base(Day.Eleven, _debugging) { }
 
     public override async Task Run()
     {
-        var pythonExe = @"C:\Users\JoevandeBilt\AppData\Local\Microsoft\WindowsApps\python.exe";
-        var scriptPath = Path.Combine(GetCurrentFilePath(), "DayEleven.py");
+        var linesOfInput = await LoadFile();
+        Dictionary<long, long> stones = new();
 
-        ProcessStartInfo startInfo = new ProcessStartInfo
+        foreach (var line in linesOfInput)
         {
-            FileName = pythonExe,
-            Arguments = scriptPath,
-            WorkingDirectory = GetCurrentFilePath(),
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
-
-        using (Process pythonScript = Process.Start(startInfo)!)
-        {
-            string result = pythonScript!.StandardOutput.ReadToEnd();
-            var parts = result.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-
-            SetResult1(long.Parse(parts.First()));
-            SetResult2(long.Parse(parts.Last()));
+            foreach (var stone in line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            {
+                stones.Add(long.Parse(stone), 1);
+            }
         }
 
+        int blink = 0;
+        while (blink < 75)
+        {
+            if (blink == 25)
+                SetResult1(stones.Sum(sc => sc.Value));
+
+            Dictionary<long, long> newStones = new();
+
+            foreach (var stone in stones)
+            {
+                if (stone.Key == 0)
+                {
+                    newStones.UpsertEntry(1, stone.Value);
+                }
+                else if (new string(stone.Key.ToString()) is var stoneText && stoneText.Length % 2 == 0)
+                {
+                    var leftPart = long.Parse(stoneText.Substring(0, (stoneText.Length / 2)));
+                    var rightPart = long.Parse(stoneText.Substring(stoneText.Length / 2));
+
+                    newStones.UpsertEntry(leftPart, stone.Value);
+                    newStones.UpsertEntry(rightPart, stone.Value);
+                }
+                else
+                {
+                    newStones.UpsertEntry(stone.Key * 2024, stone.Value);
+                }
+            }
+
+            blink++;
+            stones = newStones;
+        }
+        SetResult2(stones.Sum(sc => sc.Value));
 
         await base.Run();
     }
