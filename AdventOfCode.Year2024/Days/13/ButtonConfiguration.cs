771cd2bfad916816aa17a02faf1b56d07d4c7ce3
Joe van de Bilt <joevandebilt@live.co.uk>
2025-10-06 22:28:33 +0100

    Day 13 Parts 1 & 2 Solutions

---
+++ AdventOfCode.Year2024/Days/13/ButtonConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Year2024.Days.DayThirteen;
+
+public class ButtonConfiguration
+{
+    public char Identifier { get; set; }
+    public int XMove { get; set; }
+    public int YMove { get; set; }
+    public int Cost => Identifier == 'A' ? 3 : 1;
+}
---
+++ AdventOfCode.Year2024/Days/13/ClawMachine.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Year2024.Days.DayThirteen;
+
+public class ClawMachine
+{
+    public ButtonConfiguration AButton { get; set; } = null!;
+    public ButtonConfiguration BButton { get; set; } = null!;
+    public Prize Prize { get; set; } = null!;
+    public bool Possible { get; set; } = false;
+    public bool Big { get; set; } = false;
+    
+    public long Cost { get; set; }
+}
