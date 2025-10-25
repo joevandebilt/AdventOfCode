4cdf1b1a2fdd3c10bc7aa8a99f144a4b0fa2a261
Joe van de Bilt <joevandebilt@live.co.uk>
2025-10-25 19:21:35 +0100

    Advent of Code 2015 Days 1 & 2

---
+++ AdventOfCode.Year2015/Days/1/DayOneMain.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode.Shared.Base;
+using AdventOfCode.Shared.Enums;
+
+namespace AdventOfCode.Year2015.Days.DayOne;
+
+public class DayOneMain : AdventOfCodeDay
+{
+    private const bool _debugging = true;
+
+    public DayOneMain() : base(Day.One, _debugging) { }
+    public override async Task Run()
+    {
+        var linesOfInput = await LoadFile();
+
+        int floor = 0;
+        int position = -1;
+
+        foreach (var line in linesOfInput)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                var c = line[i];
+                if (c == '(')
+                    floor++;
+                else if (c == ')')
+                    floor--;
+
+                if (floor == -1 && position == -1)
+                    position = i + 1;
+            }
+        }
+
+        SetResult1(floor);
+        SetResult2(position);
+
+        await base.Run();
+    }
+}
---
+++ AdventOfCode.Year2015/Days/2/DayTwoMain.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Shared.Base;
+using AdventOfCode.Shared.Enums;
+
+namespace AdventOfCode.Year2015.Days.DayOne;
+
+public class DayTwoMain : AdventOfCodeDay
+{
+    private const bool _debugging = true;
+
+    public DayTwoMain() : base(Day.Two, _debugging) { }
+    public override async Task Run()
+    {
+        var linesOfInput = await LoadFile();
+        var totalPaper = 0;
+        var totalRibbon = 0;
+
+        foreach (var line in linesOfInput)
+        {
+            var numbers = line.Split('x').Select(int.Parse).OrderBy(n => n).ToArray();
+
+            var area = 2 * ((numbers[0] * numbers[1]) + (numbers[1] * numbers[2]) + (numbers[0] * numbers[2]));
+            var spare = numbers.Take(2).Aggregate(1, (a, b) => a * b);
+            var ribbon = numbers.Take(2).Aggregate((a, b) => (a * 2) + (b * 2));
+            var bow = (numbers[0] * numbers[1] * numbers[2]);
+
+            totalPaper += area + spare;
+            totalRibbon += bow + ribbon;
+        }
+
+        SetResult1(totalPaper);
+        SetResult2(totalRibbon);
+
+        await base.Run();
+    }
+}
