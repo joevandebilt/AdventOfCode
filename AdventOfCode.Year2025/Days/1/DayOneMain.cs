5498b136103aab49d32d89eefc4331dc180d9e55
Joe van de Bilt <joevandebilt@live.co.uk>
2025-12-01 23:12:35 +0000

    2025 Day 1 Part 1 & 2

---
+++ AdventOfCode.Year2025/Days/1/DayOneMain.cs
@@ -0,0 +1,76 @@
+﻿using AdventOfCode.Shared.Base;
+using AdventOfCode.Shared.Enums;
+using System.Numerics;
+
+namespace AdventOfCode.Shared.Template;
+public class DayOneMain : AdventOfCodeDay
+{
+    private const bool _debugging = true;
+    public DayOneMain() : base(Day.One, _debugging) { }
+
+    public override async Task Run()
+    {
+        var linesOfInput = await LoadFile();
+
+        int dialShown = 50;
+        int hits = 0;
+        int clicks = 0;
+
+        bool freeMove = false;
+        foreach (var line in linesOfInput)
+        {
+            //Process each line here
+            int invert = 1;
+            if (line.StartsWith('l'))
+                invert = -1;
+
+            var numberPart = int.Parse(line[1..]);
+            clicks += numberPart / 100;
+            
+            var shift = numberPart % 100;
+
+            dialShown += shift * invert;
+            
+            if (dialShown == 0)
+            {
+                hits++;
+                clicks++;
+                freeMove = true;
+            }
+            else if (dialShown == 100)
+            {
+                hits++;
+                clicks++;
+                dialShown = 0;
+                freeMove = true;
+            }
+            else if (dialShown > 100)
+            {
+                dialShown -= 100;
+                if (!freeMove)
+                    clicks++;
+                else
+                    freeMove = false;
+            }
+            else if (dialShown < 0)
+            {
+                dialShown += 100;
+                if (!freeMove)
+                    clicks++;
+                else
+                    freeMove = false;
+            }
+            else if (freeMove)
+            {
+                freeMove = false;
+            }
+
+                WriteLine($"{line} -> {dialShown}");
+        }
+
+
+        SetResult1(hits);
+        SetResult2(clicks);
+        await base.Run();
+    }
+}
