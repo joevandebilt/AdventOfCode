d0a6e34fa316619ef96d88dfbf0b4478cd0f5384
Joe van de Bilt <joe.van.de.bilt@codewizards.co.uk>
2025-12-02 09:28:16 +0000

    2025 Day 2 Part 1 & 2

---
+++ AdventOfCode.Year2025/Days/2/DayTwoMain.cs
@@ -0,0 +1,68 @@
+﻿using AdventOfCode.Shared.Base;
+using AdventOfCode.Shared.Enums;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2025.Days.DayTwo;
+
+public class DayTwoMain : AdventOfCodeDay
+{
+    private const bool _debugging = true;
+    public DayTwoMain() : base(Day.Two, _debugging) { }
+
+    public override async Task Run()
+    {
+        var linesOfInput = await LoadFile();
+        var part1Ids = new List<long>();
+        var part2Ids = new List<long>();
+
+        foreach (var line in linesOfInput)
+        {
+            //Process each line here
+            foreach (var productRange in line.Split(',', StringSplitOptions.RemoveEmptyEntries))
+            {
+                var productIds = productRange.Split('-').Select(x => long.Parse(x));
+                var (part1, part2) = ScanValidRange(productIds.First(), productIds.Last());
+
+                part1Ids.AddRange(part1);
+                part2Ids.AddRange(part2);
+            }
+        }
+
+        SetResult1(part1Ids.Sum());
+        SetResult2(part2Ids.Sum());
+        await base.Run();
+    }
+
+    private (IEnumerable<long>, IEnumerable<long>) ScanValidRange(long start, long end)
+    {
+        List<long> repeatingHalves = new();
+        List<long> repeatingSequence = new();
+
+        //An invalid Id must repeat itself so must start at twice it's length
+        for (long i = start; i <= end; i++)
+        {
+            var idString = i.ToString();
+            var half = idString.Length / 2;
+
+            for (int j = 1; j <= half; j++)
+            {
+                if (idString.Length % j != 0)
+                    continue;
+
+                var chunks = idString.Chunk(j).Select(x => new string(x)).ToList();
+                var uniqueEntries = chunks.Distinct().ToList();
+                if (uniqueEntries.Count == 1)
+                {
+                    if (!repeatingSequence.Contains(i))
+                        repeatingSequence.Add(i);                    
+                    
+                    if (j == half && idString.Length % 2 == 0)
+                        repeatingHalves.Add(i);
+                }
+            }
+
+        }
+
+        return (repeatingHalves, repeatingSequence);
+    }
+}
